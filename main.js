4e6ed7f9591c9e0c3b7804c0f48212d626a24e1f
rfoxinter <rfoxinter@gmail.com>
2022-11-02 22:49:03 +0100

    Update main.js

--- main.js
+++ main.js
@@ -29,31 +29,12 @@ async function display_files(sitemap,i){
     }
 
     const c = document.getElementsByTagName('main')[0];
-    await load_files(files);
     for (var i=0; i<files.length; ++i) {
-        display_file_number(files,i,c,path);
+        await display_file_number(files[i],c,path);
     }
 }
 
-async function load_files(files){
-    for (var i=0; i<files.length; ++i) {
-        var f = files[i];
-        var file = new XMLHttpRequest();
-        file.open('GET', 'https://rfoxinter.github.io/revisions/input/' + f.substring(f.search('/')+1,f.length).replace(/[0-9]/g, '') + '.txt');
-        file.send();
-        file.onreadystatechange = function() {
-            if (file.readyState === 4) {
-                if (file.status === 200) {
-                    file.responseText;
-                }
-            }
-        }
-    }
-    return;
-}
-
-async function display_file_number(files,i,c,path) {
-    var f = files[i];
+async function display_file_number(f,c,path) {
     var file = new XMLHttpRequest();
     file.open('GET', 'https://rfoxinter.github.io/revisions/input/' + f.substring(f.search('/')+1,f.length).replace(/[0-9]/g, '') + '.txt');
     file.send();
@@ -89,11 +70,12 @@ async function display_file_number(files,i,c,path) {
                 }
                 p = document.createElement('p');
                 var a = document.createElement('a');
-                a.setAttribute('href',path + files[i] + '.pdf');
+                a.setAttribute('href',path + f + '.pdf');
                 a.innerHTML = filename;
                 p.appendChild(a);
                 document.getElementsByClassName(groupname)[0].appendChild(p);
             }
         }
     }
+    return;
 }
