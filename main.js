13f5e4ab7d7d0d19958ba722bfeedf284273a8ab
rfoxinter <rfoxinter@gmail.com>
2022-11-03 10:50:21 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function is_numeric(str){
     return /^\d+$/.test(str);
 }
 
-async function display() {
+function display() {
     var file = new XMLHttpRequest();
     file.open('GET', 'https://rfoxinter.github.io/revisions/sitemap.xml');
     file.send();
@@ -15,7 +15,7 @@ async function display() {
     }
 }
 
-async function display_files(sitemap,i){
+function display_files(sitemap,i){
     const path = 'https://rfoxinter.github.io/revisions/pdf/';
     const extension = '\\.pdf';
 
@@ -29,10 +29,47 @@ async function display_files(sitemap,i){
     }
 
     const c = document.getElementsByTagName('main')[0];
-    display_file_number(files,0,c,path);
+    const l = files.length;
+    
+    for (var i=0; i<l; ++i) {
+        var f = files[i];
+        var filename = '';
+        var groupname = '';
+        var idx = f.search('/');
+        if (idx == -1) {
+            groupname = 'None';
+            filename = f;
+        } else {
+            groupname = f.substring(0,idx);
+            filename = f.substring(idx+1,f.length);
+        }
+        if (document.getElementsByClassName(groupname).length == 0) {
+            d = document.createElement('div');
+            d.setAttribute('class',groupname);
+            d.setAttribute('style','margin-left: 2rem;');
+            h1 = document.createElement('h1');
+            h1.setAttribute('style','margin-bottom:0;margin-left: -2rem;')
+            span = document.createElement('span');
+            span.setAttribute('class',"material-symbols-rounded");
+            span.innerHTML = 'folder_open';
+            h1.appendChild(span);
+            h1.innerHTML += ' ' + groupname;
+            d.appendChild(h1);
+            hr = document.createElement('hr');
+            c.appendChild(hr)
+            c.appendChild(d);
+        }
+        p = document.createElement('p');
+        var a = document.createElement('a');
+        a.setAttribute('href',path + f + '.pdf');
+        a.innerHTML = filename + '.pdf';
+        p.appendChild(a);
+        document.getElementsByClassName(groupname)[0].appendChild(p);
+    }
+    // display_file_number(files,0,c,path);
 }
 
-function display_file_number(files,i,c,path) {
+/*function display_file_number(files,i,c,path) {
     var f = files[i];
     var file = new XMLHttpRequest();
     file.open('GET', 'https://rfoxinter.github.io/revisions/input/' + f.substring(f.search('/')+1,f.length).replace(/[0-9]/g, '') + '.txt');
@@ -79,4 +116,4 @@ function display_file_number(files,i,c,path) {
             }
         }
     }
-}
+}*/
