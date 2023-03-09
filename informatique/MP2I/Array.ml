b9158472e5dabe532cae1624cc3e937b995430e1
rfoxinter <rfoxinter@gmail.com>
2023-03-09 20:45:33 +0100

    Delete Array.ml

--- informatique/MP2I/Array.ml
+++
@@ -1,9 +0,0 @@
-let length arr =
-    let l = ref 0 and count = ref true in
-    while !count do
-        try
-            let _ = arr.(!l) in incr l
-        with
-            | Invalid_argument _ -> count := false
-    done;
-    !l;;
