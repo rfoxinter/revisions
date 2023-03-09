dd2fde1b407f67d44dfb9ab337d9f4a57fb57e0b
rfoxinter <rfoxinter@gmail.com>
2023-03-09 14:06:05 +0100

    Update Flashcards.py

--- Flashcards.py
+++ Flashcards.py
@@ -1,19 +1,19 @@
 from random import shuffle as sh
-from subprocess import run as s
-from subprocess import PIPE,CalledProcessError
+from subprocess import run as r
+from subprocess import PIPE
 from os import chdir as c
 from os import remove as rem
 from os import rename as ren
 from os import makedirs as m
 from os import listdir as l
-from os.path import realpath,dirname
+from os.path import realpath as r
 from os.path import dirname as d
 from os.path import exists as e
 from os.path import splitext as sp
 from platform import system
-from re import sub
+from re import sub as sb
 from time import sleep as sl
-import argparse
+from argparse import ArgumentParser
 
 def b(a:str) -> bool:
     if 'True' in a:
@@ -22,28 +22,28 @@ def b(a:str) -> bool:
         return False
     raise ValueError('The value is not a Boolean')
 
-parser=argparse.ArgumentParser()
-parser.add_argument('--file',type=str,help='File to compile',default='')
-parser.add_argument('--n',type=int,help='Number of flascards',default=1)
-parser.add_argument('--dest',type=str,help='Destination folder',default='default')
-parser.add_argument('--open',type=b,help='Open destination folder after generating pdfs',default='False')
-args=parser.parse_args()
+p = ArgumentParser()
+p.add_argument('--file', type = str, help = 'File to compile', default = '')
+p.add_argument('--n', type = int, help = 'Number of flascards', default = 1)
+p.add_argument('--dest', type = str, help = 'Destination folder', default = 'default')
+p.add_argument('--open', type = b, help = 'Open destination folder after generating pdfs', default = 'False')
+args = p.parse_args()
 
-def remove_special_chars(string:str) -> str:
-    string=sub(u'[ ]','',string)
-    string=sub(u'[àáâä]','a',string)
-    string=sub(u'[èéêë]','e',string)
-    string=sub(u'[îï]','i',string)
-    string=sub(u'[ôö]','o',string)
-    string=sub(u'[ùúûü]','u',string)
-    string=sub(u'[ç]','c',string)
-    string=sub(u'[ÀÁÂÄ]','A',string)
-    string=sub(u'[ÈÉÊË]','E',string)
-    string=sub(u'[ÎÏ]','I',string)
-    string=sub(u'[ÔÖ]','O',string)
-    string=sub(u'[ÙÚÛÜ]','U',string)
-    string=sub(u'[Ç]','C',string)
-    return string
+def remove_special_chars(s:str) -> str:
+    s = sb(u'[ ]', '', s)
+    s = sb(u'[àáâä]', 'a', s)
+    s = sb(u'[èéêë]', 'e', s)
+    s = sb(u'[îï]', 'i', s)
+    s = sb(u'[ôö]', 'o', s)
+    s = sb(u'[ùúûü]', 'u', s)
+    s = sb(u'[ç]', 'c', s)
+    s = sb(u'[ÀÁÂÄ]', 'A', s)
+    s = sb(u'[ÈÉÊË]', 'E', s)
+    s = sb(u'[ÎÏ]', 'I', s)
+    s = sb(u'[ÔÖ]', 'O', s)
+    s = sb(u'[ÙÚÛÜ]', 'U', s)
+    s = sb(u'[Ç]', 'C', s)
+    return s
 
 def output_f(files:list) -> list:
     ret = []
@@ -58,7 +58,7 @@ def output_f(files:list) -> list:
         if len(dest) == 1:
             dest = 'pdf/'
         else:
-            dest = 'pdf/'+remove_special_chars(dest[0].title())+'/'
+            dest = 'pdf/' + remove_special_chars(dest[0].title()) + '/'
         ret.append(dest)
     return ret
 
@@ -71,21 +71,21 @@ def recompile(dest:str) -> bool:
     for i in range(len(files)):
         c(d(__file__))
         c(d('output/'))
-        ttle = files[i].replace('.tex','')
+        ttle = files[i].replace('.tex', '')
         dest = output[i]
-        print('Compilation in progress [' + str(i+1) + '/' + str(len(files)) + ']\nCompilation of [' + files[i] + ']\n')
-        out = s('latexmk -synctex=1 -interaction=nonstopmode -file-line-error -pdf '+ ttle, shell = True, stdout = PIPE, stderr = PIPE, text=True).stdout
+        print('Compilation in progress [' + str(i + 1) + '/' + str(len(files)) + ']\nCompilation of [' + files[i] + ']\n')
+        out = r('latexmk -synctex=1 -interaction=nonstopmode -file-line-error -pdf ' + ttle, shell = True, stdout = PIPE, stderr = PIPE, text = True).stdout
         if 'Command for \'pdflatex\' gave return code 1' in out:
             print(out + '\n')
             fail = True
         elif not 'Output written on' in out:
-            out = s('pdflatex -interaction=nonstopmode -file-line-error '+ ttle, shell = True, stdout = PIPE, stderr = PIPE, text=True, check=True).stdout
+            out = r('pdflatex -interaction=nonstopmode -file-line-error ' + ttle, shell = True, stdout = PIPE, stderr = PIPE, text = True, check = True).stdout
             if 'Command for \'pdflatex\' gave return code 1' in out:
                 print(out + '\n')
                 fail = True
         if e('../' + ttle + '.pdf'):
             rem('../' + ttle + '.pdf')
-        ext = ['.aux','.fdb_latexmk','.fls','.log','.nav','.out','.snm','.synctex.gz','.toc']
+        ext = ['.aux', '.fdb_latexmk', '.fls', '.log', '.nav', '.out', '.snm', '.synctex.gz', '.toc']
         for i in ext:
             try:
                 rem(ttle + i)
@@ -103,16 +103,16 @@ def recompile(dest:str) -> bool:
             fail = True
     return fail
 
-def gen_latex(r:list,t:str,ttle:str,dest:str,num:str='') -> bool:
+def gen_latex(r:list, t:str, ttle:str, dest:str, num:str = '') -> bool:
     c(d(__file__))
-    sh_quest,sh_qr,defs=b(r[1]),b(r[2]),r[3]
-    r = [r[i].split(';;') for i in range(4,len(r))]
+    sh_quest, sh_qr, defs = b(r[1]), b(r[2]), r[3]
+    r = [r[i].split(';;') for i in range(4, len(r))]
     if sh_qr:
         for i in r:
             sh(i)
     if sh_quest:
         sh(r)
-    del sh_qr,sh_quest
+    del sh_qr, sh_quest
     t2 = t.split(' -- ')
     if len(t2)==1:
         t2 = t2[0]
@@ -131,33 +131,33 @@ def gen_latex(r:list,t:str,ttle:str,dest:str,num:str='') -> bool:
     \\titlepage
 \\end{frame}
 '''
-    del defs,t2
+    del defs, t2
     for i in range(len(r)):
         if r[i][0][0:5]=='!!fst':
-            r[i][0] = r[i][0].replace('!!fst','')
+            r[i][0] = r[i][0].replace('!!fst', '')
         elif r[i][1][0:5]=='!!fst':
-            r[i][0],r[i][1] = r[i][1].replace('!!fst',''),r[i][0]
-        file += '\\slideq{' + r[i][0] + '}{' + str(i+1) + '/' + str(len(r)) + '}\n'
-        file += '\\slider{' + r[i][1] + '}{' + str(i+1) + '/' + str(len(r)) + '}\n'
+            r[i][0], r[i][1] = r[i][1].replace('!!fst', ''), r[i][0]
+        file += '\\slideq{' + r[i][0] + '}{' + str(i + 1) + '/' + str(len(r)) + '}\n'
+        file += '\\slider{' + r[i][1] + '}{' + str(i + 1) + '/' + str(len(r)) + '}\n'
     file += '\end{document}'
-    f = open('output/' + ttle + '.tex','w')
+    f = open('output/' + ttle + '.tex', 'w')
     f.write(file)
     f.close()
     c('output/')
     fail = False
     print('Compilation in progress' + num + '\n')
-    output = s('latexmk -synctex=1 -interaction=nonstopmode -file-line-error -pdf '+ ttle, shell = True, stdout = PIPE, stderr = PIPE, text=True).stdout
+    output = r('latexmk -synctex=1 -interaction=nonstopmode -file-line-error -pdf ' + ttle, shell = True, stdout = PIPE, stderr = PIPE, text = True).stdout
     if 'Command for \'pdflatex\' gave return code 1' in output:
         print(output + '\n')
         fail = True
     elif not 'Output written on' in output:
-        output = s('pdflatex -interaction=nonstopmode -file-line-error '+ ttle, shell = True, stdout = PIPE, stderr = PIPE, text=True, check=True).stdout
+        output = r('pdflatex -interaction=nonstopmode -file-line-error ' + ttle, shell = True, stdout = PIPE, stderr = PIPE, text = True, check = True).stdout
         if 'Command for \'pdflatex\' gave return code 1' in output:
             print(output + '\n')
             fail = True
     if e('../' + ttle + '.pdf'):
         rem('../' + ttle + '.pdf')
-    ext = ['.aux','.fdb_latexmk','.fls','.log','.nav','.out','.snm','.synctex.gz','.toc']
+    ext = ['.aux', '.fdb_latexmk', '.fls', '.log', '.nav', '.out', '.snm', '.synctex.gz', '.toc']
     for i in ext:
         try:
             rem(ttle + i)
@@ -175,16 +175,16 @@ def gen_latex(r:list,t:str,ttle:str,dest:str,num:str='') -> bool:
         fail = True
     return fail
 
-def main(file_path:str,file:str,n:int,dest:str,_open:bool) -> bool:
+def main(file_path:str, file:str, n:int, dest:str, _open:bool) -> bool:
     c(d(__file__))
     fail = False
     if file == '__recompile__':
         return recompile(dest)
-    c(dirname(realpath(file_path)))
+    c(d(r(file_path)))
     if file == '':
         file = input('File to compile : ')
     try:
-        r = open('input/'+file+'.txt', 'r').read().split('\n')
+        r = open('input/' + file + '.txt', 'r').read().split('\n')
     except OSError:
         raise RuntimeError('File not found')
     t, ttle = '', ''
@@ -203,23 +203,23 @@ def main(file_path:str,file:str,n:int,dest:str,_open:bool) -> bool:
         if len(dest) == 1:
             dest = 'pdf/'
         else:
-            dest = 'pdf/'+remove_special_chars(dest[0].title())+'/'
+            dest = 'pdf/' + remove_special_chars(dest[0].title()) + '/'
     if n == 1:
-        fail = gen_latex(r,t,ttle,dest)
+        fail = gen_latex(r, t, ttle, dest)
     else:
         for j in range(n):
-            fail = gen_latex(r,t,ttle+'.'+str(j+1),dest,' [' + str (j+1) + '/' + str(n) + ']') and fail
+            fail = gen_latex(r, t, ttle + '.' + str(j + 1), dest, ' [' + str (j + 1) + '/' + str(n) + ']') and fail
     if _open:
         sl(.5)
         if system() == 'Windows':
-            if s('start ' + dest, shell = True, stdout = PIPE, stderr = PIPE, text=True).stdout != '':
+            if r('start ' + dest, shell = True, stdout = PIPE, stderr = PIPE, text = True).stdout != '':
                 print('Unable to open the folder\n')
         else:
-            if s('open ' + dest, shell = True, stdout = PIPE, stderr = PIPE, text=True).stdout != '':
+            if r('open ' + dest, shell = True, stdout = PIPE, stderr = PIPE, text = True).stdout != '':
                 print('Unable to open the folder\n')
     return fail
 
-if __name__=='__main__':
-    if main(__file__,sp(args.file)[0],args.n,args.dest,args.open):
+if __name__ == '__main__':
+    if main(__file__, sp(args.file)[0], args.n, args.dest, args.open):
         raise RuntimeError('An error has occurred')
     print('Compilation complete')
