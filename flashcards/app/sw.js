b62780afc96fa4aad102a453efd89f63a3f99758
rfoxinter <rfoxinter@gmail.com>
2023-07-22 18:51:18 +0200

    Create sw.js

---
+++ flashcards/app/sw.js
@@ -0,0 +1,54 @@
+var APP_PREFIX = 'Flashcards'     // Identifier for this app (this needs to be consistent across every cache update)
+var VERSION = '1'              // Version of the off-line cache (change this value everytime you want to update cache)
+var CACHE_NAME = APP_PREFIX + VERSION
+var URLS = [                            // Add URL you want to cache in this list.
+  './',                     // If you have separate JS/CSS files,
+  './index.html',           // add path to those files here
+  './viewer.html'
+]
+
+// Respond with cached resources
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      if (request) { // if cache is available, respond with cache
+        return request
+      } else {       // if there are no cache, try fetching request
+        return fetch(e.request)
+      }
+
+      // You can omit if/else for console.log & put one line below like this too.
+      // return request || fetch(e.request)
+    })
+  )
+})
+
+// Cache resources
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(URLS)
+    })
+  )
+})
+
+// Delete outdated caches
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      // `keyList` contains all cache names under your username.github.io
+      // filter out ones that has this app prefix to create white list
+      var cacheWhitelist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX)
+      })
+      // add current cache name to white list
+      cacheWhitelist.push(CACHE_NAME)
+
+      return Promise.all(keyList.map(function (key, i) {
+        if (cacheWhitelist.indexOf(key) === -1) {
+          return caches.delete(keyList[i])
+        }
+      }))
+    })
+  )
+})
