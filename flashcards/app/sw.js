223abb5908a09a4b84e7b23ce313c84147408cd0
rfoxinter <rfoxinter@gmail.com>
2023-07-23 13:21:17 +0200

    Update sw.js

--- flashcards/app/sw.js
+++ flashcards/app/sw.js
@@ -2,6 +2,7 @@ var APP_PREFIX = 'Flashcards'     // Identifier for this app (this needs to be c
 var VERSION = '1'              // Version of the off-line cache (change this value everytime you want to update cache)
 var CACHE_NAME = APP_PREFIX + VERSION
 var URLS = [                            // Add URL you want to cache in this list.
+  './',
   './index.html',
   './huffman.js',
   './main.js',
