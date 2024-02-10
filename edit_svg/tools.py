72e462b3ae2df0e92d678166ed1c53139174ad0a
rfoxinter <rfoxinter@gmail.com>
2024-02-10 22:10:51 +0100

    Unslant partial

--- edit_svg/tools.py
+++ edit_svg/tools.py
@@ -4,9 +4,16 @@ from re import sub
 def get_text(line: str) -> list[str]:
     return list(filter(lambda x: x  != '', map(lambda x: sub('>', '', sub('<', '', x)), findall('>.*?<', line))))
 
+def get_x(line: str) -> float:
+    return float(sub('x=', '', sub('\'', '', findall('x=\'-?[0-9]*\.?[0-9]*\'', line)[0])))
+
 def get_y(line: str) -> float:
     return float(sub('y=', '', sub('\'', '', findall('y=\'-?[0-9]*\.?[0-9]*\'', line)[0])))
 
 def get_transform(line: str) -> tuple[str, tuple[float, float]]:
     transform = findall('transform=\'matrix\(.*?\)\'', line)[0]
     return transform, list(map(float, sub('transform=\'matrix\(', '', sub('\)\'', '', transform)).split(' ')[4:6]))
+
+def get_tspan(line: str) -> tuple[str, tuple[float, float]]:
+    tspan = findall('<tspan.*?</tspan>', line)[0]
+    return (get_text(tspan)[0], (get_x(tspan), get_y(tspan)))
--- edit_svg/unslantpartial.py
+++ edit_svg/unslantpartial.py
@@ -8,17 +8,28 @@ def line_transform(line: str) -> str:
     if not '∂' in text:
         return line
     if not 'transform' in line:
+        if not '∂</tspan>' in line:
+            if len(text) == 1:
+                return sub('>', ' transform=\'matrix(1 0 0.25 1 ' + str(round(-tools.get_y(line) / 4, 6)) + ' 0)\'>', line, count=1)
+            l1 = line[0:search('∂', line).start() + 1] + '</text>'
+            l2 = sub('∂', '', line)
+            return sub('>', ' transform=\'matrix(1 0 0.25 1 ' + str(round(-tools.get_y(line) / 4, 6)) + ' 0)\'>', l1, count=1) + '\n' + l2
         if len(text) == 1:
             return sub('>', ' transform=\'matrix(1 0 0.25 1 ' + str(round(-tools.get_y(line) / 4, 6)) + ' 0)\'>', line, count=1)
+        l1 = sub('>.*?<tspan', '><tspan', line[0:search('∂', line).start() + 1], count=1) + '</tspan></text>'
+        (tspan, (x, y)) = tools.get_tspan(l1)
+        l1 = sub('y=\'-?[0-9]*\.?[0-9]*\'', 'y=\'' + str(y) + '\'', sub('x=\'-?[0-9]*\.?[0-9]*\'', 'x=\'' + str(x) + '\'', l1))
+        l2 = sub('∂', '', line)
+        return sub('>', ' transform=\'matrix(1 0 0.25 1 ' + str(round((-tools.get_y(line) - tools.get_x(l2) + x) / 4, 6)) + ' 0)\'>', sub('<tspan.*?</tspan>', tspan, l1), count=1) + '\n' + l2
+    if not '∂</tspan>' in line:
+        (transform, (dx, dy)) = tools.get_transform(line)
+        if len(text) == 1:
+            return sub('>', ' transform=\'matrix(1 0 0.25 1 ' + str(round(-tools.get_y(line) / 4, 6) + dx) + ' ' + str(dy) + ')\'>', sub(escape(transform), '', line, count=1), count=1)
         l1 = line[0:search('∂', line).start() + 1] + '</text>'
         l2 = sub('∂', '', line)
-        return sub('>', ' transform=\'matrix(1 0 0.25 1 ' + str(round(-tools.get_y(line) / 4, 6)) + ' 0)\'>', l1, count=1) + '\n' + l2
-    (transform, (dx, dy)) = tools.get_transform(line)
-    if len(text) == 1:
-        return sub('>', ' transform=\'matrix(1 0 0.25 1 ' + str(round(-tools.get_y(line) / 4, 6) + dx) + ' ' + str(dy) + ')\'>', sub(escape(transform), '', line, count=1), count=1)
-    l1 = line[0:search('∂', line).start() + 1] + '</text>'
-    l2 = sub('∂', '', line)
-    return sub('>', ' transform=\'matrix(1 0 0.25 1 ' + str(round(-tools.get_y(line) / 4, 6) + dx) + ' ' + str(dy) + ')\'>', sub(escape(transform), '', l1, count=1), count=1) + '\n' + l2
+        return sub('>', ' transform=\'matrix(1 0 0.25 1 ' + str(round(-tools.get_y(line) / 4, 6) + dx) + ' ' + str(dy) + ')\'>', sub(escape(transform), '', l1, count=1), count=1) + '\n' + l2
+    print('∂ left unchanged in ' + line)
+    return line
 
 def main(flnm: str) -> None:
     f = open(flnm, 'r', encoding='UTF-8').readlines()
