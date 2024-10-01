c4f5bb78321f068d2467381325016fb135228e24
rfoxinter <rfoxinter@gmail.com>
2024-10-01 08:51:56 +0200

    Espaces Préhilbertiens réels

--- edit_svg/unslantpartial.py
+++ edit_svg/unslantpartial.py
@@ -18,7 +18,7 @@ def line_transform(line: str) -> str:
             return sub('>', ' transform=\'matrix(1 0 0.25 1 ' + str(round(-tools.get_y(line) / 4, 6)) + ' 0)\'>', line, count=1)
         l1 = sub('>.*?<tspan', '><tspan', line[0:search('∂', line).start() + 1], count=1) + '</tspan></text>'
         (tspan, (x, y)) = tools.get_tspan(l1)
-        l1 = sub('y=\'-?[0-9]*\.?[0-9]*\'', 'y=\'' + str(y) + '\'', sub('x=\'-?[0-9]*\.?[0-9]*\'', 'x=\'' + str(x) + '\'', l1))
+        l1 = sub('y=\'-?[0-9]*.?[0-9]*\'', 'y=\'' + str(y) + '\'', sub('x=\'-?[0-9]*.?[0-9]*\'', 'x=\'' + str(x) + '\'', l1))
         l2 = sub('∂', '', line)
         return sub('>', ' transform=\'matrix(1 0 0.25 1 ' + str(round((-tools.get_y(line) - tools.get_x(l2) + x) / 4, 6)) + ' 0)\'>', sub('<tspan.*?</tspan>', tspan, l1), count=1) + '\n' + l2
     if not '∂</tspan>' in line:
