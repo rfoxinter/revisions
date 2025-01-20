4ee0fabc7c3413f1265519939aa5b4e198ca52f2
rfoxinter <rfoxinter@gmail.com>
2025-01-20 22:38:37 +0100

    Update directory

---
+++ L3_anglais/CardsList.py
@@ -0,0 +1,30 @@
+from os import listdir as l
+from os import chdir as c
+from os.path import dirname as d
+from os.path import exists as e
+from os.path import isdir as i
+from os.path import getmtime as g
+from datetime import datetime as dt
+
+c(d(__file__) + '/flashcards')
+f = open('./cards.txt', 'w')
+content = 'https://rfoxinter.github.io/revisions/flashcards/'
+global cards
+cards = []
+def list_f(folder):
+    global cards
+    curr_cards = []
+    for sub_folder in sorted(l(folder), reverse=True):
+        if e(folder + '/' + sub_folder + '/info.txt'):
+            if folder == '.':
+                curr_cards.append('./' + sub_folder)
+            else:
+                curr_cards.append(folder + '/' + sub_folder)
+        elif i(folder + '/' + sub_folder):
+            list_f(folder + '/' + sub_folder)
+    cards = sorted(curr_cards) + cards
+list_f('.')
+for c in cards:
+    content += '\n../L3S1/anglais/flashcards/' + c.replace('./', '') + '\n' + dt.fromtimestamp(g(c)).strftime('%Y%m%d%H%M%S')
+f.write(content)
+f.close()
