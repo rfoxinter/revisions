7463de2b1f80a0b0acc565c86eefcd723f6b2b92
rfoxinter <rfoxinter@gmail.com>
2025-01-23 16:15:01 +0100

    Cours EDP

--- edit_svg/tools.py
+++ edit_svg/tools.py
@@ -4,16 +4,22 @@ from re import sub
 def get_text(line: str) -> list[str]:
     return list(filter(lambda x: x  != '', map(lambda x: sub('>', '', sub('<', '', x)), findall('>.*?<', line))))
 
-def get_x(line: str) -> float:
-    return float(sub('x=', '', sub('\'', '', findall(r'x=\'-?[0-9]*\.?[0-9]*\'', line)[0])))
+def get_x(line: str, line_x: float = 0.0) -> float:
+    try:
+        return float(sub('x=', '', sub('\'', '', findall(r'x=\'-?[0-9]*\.?[0-9]*\'', line)[0])))
+    except:
+        return line_x
 
-def get_y(line: str) -> float:
-    return float(sub('y=', '', sub('\'', '', findall(r'y=\'-?[0-9]*\.?[0-9]*\'', line)[0])))
+def get_y(line: str, line_y: float = 0.0) -> float:
+    try:
+        return float(sub('y=', '', sub('\'', '', findall(r'y=\'-?[0-9]*\.?[0-9]*\'', line)[0])))
+    except:
+        return line_y
 
 def get_transform(line: str) -> tuple[str, tuple[float, float]]:
     transform = findall(r'transform=\'matrix\(.*?\)\'', line)[0]
     return transform, list(map(float, sub(r'transform=\'matrix\(', '', sub(r'\)\'', '', transform)).split(' ')[4:6]))
 
-def get_tspan(line: str) -> tuple[str, tuple[float, float]]:
+def get_tspan(line: str, line_x: float = 0.0, line_y: float = 0.0) -> tuple[str, tuple[float, float]]:
     tspan = findall('<tspan.*?</tspan>', line)[0]
-    return (get_text(tspan)[0], (get_x(tspan), get_y(tspan)))
+    return (get_text(tspan)[0], (get_x(tspan, line_x), get_y(tspan, line_y)))
