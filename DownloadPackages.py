3c26771133e06563d98ce13503629aad475b797b
rfoxinter <rfoxinter@gmail.com>
2025-07-05 12:22:44 +0200

    Update DownloadPackages.py

--- DownloadPackages.py
+++ DownloadPackages.py
@@ -2,11 +2,11 @@ from urllib import request
 from os.path import exists
 from os import mkdir, chdir
 
-files = request.urlopen("https://rfoxinter.github.io/revisions/output/packages.txt").read().decode("utf-8")
+files = request.urlopen("https://raw.githubusercontent.com/rfoxinter/revisions/refs/heads/main/output/packages.txt").read().decode("utf-8")
 print(files)
 if not exists("./output"):
     mkdir("./output")
 chdir("./output")
 for file in files.splitlines():
     print(file)
-    request.urlretrieve("https://rfoxinter.github.io/revisions/output/" + file, file)
+    request.urlretrieve("https://raw.githubusercontent.com/rfoxinter/revisions/refs/heads/main/output/" + file, file)
