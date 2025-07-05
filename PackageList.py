a94896548a4e6f3f1d7f89fe380f38c84422f6ee
rfoxinter <rfoxinter@gmail.com>
2025-07-05 12:03:44 +0200

    Update packages

--- PackageList.py
+++ PackageList.py
@@ -4,11 +4,21 @@ from os.path import dirname as d
 from os.path import splitext as s
 from os.path import isdir as i
 
+def read_content(dir):
+    dirpath = ''
+    if dir != '.':
+        dirpath = dir + '/'
+    content = ''
+    for file in sorted(l(dir)):
+        if not i(file) and not s(file)[1] in ['.cls', '.tex', '.txt']:
+            content += dirpath + file + '\n'
+        elif i(file):
+            content += read_content(dirpath + file)
+    return content
+
 c(d(__file__) + '/output')
 f = open('./packages.txt', 'w')
 content = ''
-for file in sorted(l('.')):
-    if not i(file) and not s(file)[1] in ['.cls', '.tex', '.txt']:
-        content += file + '\n'
+content = read_content('.')
 f.write(content[0:len(content)-1])
 f.close()
